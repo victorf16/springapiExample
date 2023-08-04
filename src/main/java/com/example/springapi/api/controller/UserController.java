d20a6ab1af4847cdeee8b8e8a03a4b28bfe1cbf9
victor gomes do nascimento <victorf_16@hotmail.com>
2023-08-04 16:09:49 -0300

    Adicionando Mensage

--- src/main/java/com/example/springapi/api/controller/UserController.java
+++ src/main/java/com/example/springapi/api/controller/UserController.java
@@ -11,22 +11,25 @@ import java.util.Optional;
 
 @RestController
 public class UserController {
-
+    // cria um objeto do tipo UserService
     private UserService userService;
 
+    // cria um construtor para a classe UserController
     @Autowired
     public UserController(UserService userService) {
-        this.userService = userService;
+        this.userService = userService; // atribui o objeto userService ao atributo userService da classe UserController
     }
 
-    @GetMapping("/user")
+    @GetMapping("/user") // mapeia a url http://localhost:8080/user
     public User getUser(@RequestParam Integer iid) {
-    Optional user = userService.getUser(iid);
+    Optional user = userService.getUser(iid); // chama o método getUser da classe UserService e atribui o retorno a um objeto do tipo Optional
 
-    if (user.isPresent()) {
-        return (User) user.get();
+    if (user.isPresent()) { // se o objeto user não estiver vazio
+        return (User) user.get(); // retorna o objeto user
     }
-    return null;
+
+    return null; // se o objeto user estiver vazio, retorna null
+
     }
 
 
